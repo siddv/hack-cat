03d73a787fca37a97887e58eaa06533f423de088
Siddharth Vadgama <me@siddv.net>
2018-03-18 17:09:30 +0000

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'hack-cat',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  modules: [
+    ['nuxt-i18n', {
+      locales: [
+        {
+          code: 'en',
+          iso: 'en-GB',
+          name: 'English'
+        },
+        {
+          code: 'ca',
+          iso: 'ca-ES',
+          name: 'Català'
+        }
+      ],
+      defaultLocale: 'en',
+      vueI18n: {
+        messages: {
+          en: {
+            name: 'Hack.cat',
+            intro: 'Welcome to Hack.cat. Content to come later.'
+          },
+          ca: {
+            name: 'Hack.cat',
+            intro: 'Benvingut a Hack.cat. Contingut per venir després.'
+          }
+        }
+      }
+    }]
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        {{$t('name')}}
+      </h1>
+      <h2 class="subtitle">
+        {{$t('intro')}}
+      </h2>
+
+      <div class="links">
+        <nuxt-link
+          class="button--grey"
+          v-for="(locale, index) in $i18n.locales"
+          v-if="locale.code !== $i18n.locale"
+          :key="index"
+          :exact="true"
+          :to="switchLocalePath(locale.code)">
+          
+          {{ locale.name }}
+        </nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
